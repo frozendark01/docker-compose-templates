a8b893808ca2d7d574eb2ee6330ff1e842097ba4
frozendark_ <itimpactdesign@gmail.com>
2022-10-08 15:59:00 +0300

    docker install script-ubuntu server

---
+++ ubuntudocker.sh
@@ -0,0 +1,14 @@
+#!/bin/bash
+sudo apt-get install -y \
+    apt-transport-https \
+    ca-certificates \
+    curl \
+    software-properties-common
+curl -fsSL https://download.docker.com/linux/ubuntu/gpg | sudo apt-key add -
+sudo add-apt-repository "deb [arch=amd64] https://download.docker.com/linux/ubuntu focal stable"
+apt-cache policy docker-ce
+sudo apt-get update
+sudo apt install docker-ce -y
+sudo usermod -aG docker ${USER}
+sudo systemctl enable docker.service
+sudo systemctl enable containerd.service
